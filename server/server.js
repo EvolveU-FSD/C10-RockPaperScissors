88c0d0ec2fef3f2183645cfe2240c3e9ad6bdf22
Chris Desmarais <desmarais_chris@hotmail.com>
2023-04-13 18:25:08 -0600

    Finished.

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 import express from "express"
+import cors from 'cors'
 
 const PORT = process.env.PORT || 5050
 const app = express()
+app.use(cors())
 
 const choices = [
     {value: 0, text: 'rock'},
